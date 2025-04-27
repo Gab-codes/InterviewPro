47ad5905b0f30b18317d280d23d6e4a08ff83845
Temidayo Gabriel <dayotoru@gmail.com>
2025-04-27 11:29:30 -0400

    added logic to check authenticated users

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -80,3 +80,32 @@ export async function setSessionCookie(idToken: string) {
     maxAge: ONE_WEEK,
   });
 }
+
+export async function getCurrentUser(): Promise<User | null> {
+  const cookieStore = await cookies();
+  const sessionCookie = cookieStore.get("session")?.value;
+  if (!sessionCookie) {
+    return null;
+  }
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
+    if (!userRecord.exists) return null;
+
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (e) {
+    return null;
+  }
+}
+
+export async function isAuthenticated() {
+  const user = await getCurrentUser();
+
+  return !!user;
+}
